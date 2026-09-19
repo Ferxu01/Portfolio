411e58d1fa7e3b5edfb875080834d7f590f7af4d
Fernando Ortega <ferortjim@gmail.com>
2026-09-19 12:04:28 +0200

    feat: added about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
-  imports: [RouterOutlet, HeaderComponent, BannerComponent, ExperienceComponent],
+  imports: [RouterOutlet, HeaderComponent, BannerComponent, ExperienceComponent, AboutComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
